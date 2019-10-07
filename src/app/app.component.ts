97624185f233e271e13a3c40567155714e776a5a
olgax <loli8866@gmail.com>
2019-10-07 17:42:22 +0300

    fixed config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public showSearchBar: boolean;
   title = 'hello-world (test deploy with tag 0.0.0)';
 }
